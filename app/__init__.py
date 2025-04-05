22b64aaa7b0056618947ae82cd7ae33ced4ded94
stepanvalic <valicekstepan8@seznam.cz>
2025-04-05 15:57:02 +0200

    Added PORT variable to .env and configured Flask to use it

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,6 @@ load_dotenv()
 
 app = Flask(__name__, static_folder='.')
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+app.config['PORT'] = int(os.getenv('PORT', 5000))
 
 from app import routes, models
